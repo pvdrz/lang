d2909a711d89a80123f76666c7381749a3736b6c
Christian Poveda <git@pvdrz.com>
2026-06-08 19:46:48 -0500

    Improve pretty printing

--- src/ast/pretty_printer.rs
+++ src/ast/pretty_printer.rs
@@ -1,140 +1,200 @@
-use std::fmt::Write;
+use std::fmt::{self};
 
 use crate::ast::{BinOp, Expr, Literal, LiteralKind, UnOp, visitor::Visitor};
 
-pub(crate) fn pretty_print(expr: &Expr) -> String {
-    let mut pp = PrettyPrinter { buf: String::new() };
-    pp.visit_expr(expr);
-    pp.buf
-}
+pub(crate) fn pretty_print(expr: &Expr) -> impl fmt::Display + '_ {
+    struct PrettyPrintable<'a>(&'a Expr);
 
-struct PrettyPrinter {
-    buf: String,
-}
-
-impl PrettyPrinter {
-    fn parenthesize(&mut self, name: Option<&str>, f: impl for<'b> Fn(&mut Self)) {
-        let _ = self.buf.write_str("(");
-        if let Some(name) = name {
-            let _ = write!(self.buf, "{name} ");
+    impl fmt::Display for PrettyPrintable<'_> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let mut pp = PrettyPrinter {
+                f,
+                level: 0,
+                after_newline: false,
+            };
+            pp.visit_expr(self.0)
         }
-        f(self);
-        let _ = self.buf.write_str(")");
+    }
+
+    PrettyPrintable(expr)
+}
+
+struct PrettyPrinter<'a, 'b> {
+    f: &'a mut fmt::Formatter<'b>,
+    level: usize,
+    after_newline: bool,
+}
+
+impl PrettyPrinter<'_, '_> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        if self.after_newline {
+            write!(&mut self.f, "{:width$}", "", width = self.level)?;
+            self.after_newline = false;
+        }
+
+        self.f.write_str(s)
+    }
+
+    fn parenthesize(&mut self, f: impl for<'b> Fn(&mut Self) -> fmt::Result) -> fmt::Result {
+        self.write_str("(")?;
+        f(self)?;
+        self.write_str(")")
+    }
+
+    fn newline(&mut self) -> fmt::Result {
+        self.f.write_str("\n")?;
+        self.after_newline = true;
+        Ok(())
+    }
+
+    fn indent(&mut self) {
+        self.level += 2;
+    }
+
+    fn dedent(&mut self) {
+        self.level -= 2;
     }
 }
 
-impl Visitor for PrettyPrinter {
-    fn visit_ident(&mut self, ident: &super::Ident) {
-        let _ = self.buf.write_str(&ident.inner);
-        super::visitor::visit_ident(self, ident);
+impl Visitor for PrettyPrinter<'_, '_> {
+    type Err = fmt::Error;
+
+    fn visit_ident(&mut self, ident: &super::Ident) -> fmt::Result {
+        self.write_str(&ident.inner)?;
+        super::visitor::visit_ident(self, ident)
     }
 
-    fn visit_literal(&mut self, expr_lit: &Literal) {
-        let _ = match &expr_lit.kind {
-            LiteralKind::Int(int) => write!(self.buf, "{int}"),
-            LiteralKind::Float(float) => write!(self.buf, "{float}"),
-            LiteralKind::Str(string) => self.buf.write_str(string),
-            LiteralKind::True => self.buf.write_str("true"),
-            LiteralKind::False => self.buf.write_str("false"),
-        };
+    fn visit_literal(&mut self, expr_lit: &Literal) -> fmt::Result {
+        match &expr_lit.kind {
+            LiteralKind::Int(int) => write!(self.f, "{int}"),
+            LiteralKind::Float(float) => write!(self.f, "{float}"),
+            LiteralKind::Str(string) => self.write_str(string),
+            LiteralKind::True => self.write_str("true"),
+            LiteralKind::False => self.write_str("false"),
+        }
     }
 
-    fn visit_expr_case(&mut self, expr_case: &super::ExprCase) {
-        let _ = self.buf.write_str("case ");
+    fn visit_expr_case(&mut self, expr_case: &super::ExprCase) -> fmt::Result {
+        self.write_str("case ")?;
+        self.visit_expr(&expr_case.expr)?;
+        self.write_str(" do")?;
 
-        self.visit_expr(&expr_case.expr);
-
-        let _ = self.buf.write_str(" do ");
+        if expr_case.arms.is_empty() {
+            self.write_str(" ")?;
+        } else {
+            self.newline()?;
+        }
+        self.indent();
 
         for (pat, expr) in &expr_case.arms {
-            self.visit_pat(pat);
-
-            let _ = self.buf.write_str(" -> ");
-
-            self.visit_expr(expr);
-
-            let _ = self.buf.write_str(", ");
+            self.visit_pat(pat)?;
+            self.write_str(" -> ")?;
+            self.visit_expr(expr)?;
+            self.write_str(", ")?;
+            self.newline()?;
         }
 
-        let _ = self.buf.write_str("end");
+        self.dedent();
+        self.write_str("end")
     }
 
-    fn visit_expr_let(&mut self, expr_let: &super::ExprLet) {
-        let _ = self.buf.write_str("let ");
-        self.visit_ident(&expr_let.lhs);
+    fn visit_expr_let(&mut self, expr_let: &super::ExprLet) -> fmt::Result {
+        self.write_str("let ")?;
+        self.visit_ident(&expr_let.lhs)?;
 
         for ident in &expr_let.args {
-            let _ = self.buf.write_str(" ");
-            self.visit_ident(ident);
+            self.write_str(" ")?;
+            self.visit_ident(ident)?;
+        }
+        let insert_newline = matches!(&*expr_let.rhs, Expr::Let(_) | Expr::If(_) | Expr::Case(_));
+
+        if insert_newline {
+            self.write_str(" =")?;
+
+            self.newline()?;
+            self.indent();
+        } else {
+            self.write_str(" = ")?;
+            self.indent();
         }
 
-        let _ = self.buf.write_str(" = (");
-        self.visit_expr(&expr_let.rhs);
-        let _ = self.buf.write_str(") ");
-        self.visit_expr(&expr_let.body);
+        self.visit_expr(&expr_let.rhs)?;
+        self.write_str(";")?;
+
+        self.newline()?;
+        self.dedent();
+
+        self.visit_expr(&expr_let.body)?;
+
+        Ok(())
     }
 
-    fn visit_expr_unary(&mut self, expr_unary: &super::ExprUnary) {
-        self.parenthesize(None, |this| {
-            super::visitor::visit_expr_unary(this, expr_unary);
-        });
+    fn visit_expr_unary(&mut self, expr_unary: &super::ExprUnary) -> fmt::Result {
+        self.parenthesize(|this| super::visitor::visit_expr_unary(this, expr_unary))
     }
 
-    fn visit_un_op(&mut self, un_op: &UnOp) {
-        let _ = match un_op {
-            UnOp::Neg => self.buf.write_str("-"),
-            UnOp::Not => self.buf.write_str("!"),
-        };
+    fn visit_un_op(&mut self, un_op: &UnOp) -> fmt::Result {
+        match un_op {
+            UnOp::Neg => self.write_str("-"),
+            UnOp::Not => self.write_str("!"),
+        }
     }
 
-    fn visit_expr_binary(&mut self, expr_binary: &super::ExprBinary) {
-        self.parenthesize(None, |this| {
-            super::visitor::visit_expr_binary(this, expr_binary)
-        })
+    fn visit_expr_binary(&mut self, expr_binary: &super::ExprBinary) -> fmt::Result {
+        self.parenthesize(|this| super::visitor::visit_expr_binary(this, expr_binary))
     }
 
-    fn visit_bin_op(&mut self, bin_op: &BinOp) {
-        let _ = match bin_op {
-            BinOp::Eq => self.buf.write_str(" == "),
-            BinOp::Ne => self.buf.write_str(" != "),
-            BinOp::Lt => self.buf.write_str(" < "),
-            BinOp::Le => self.buf.write_str(" <= "),
-            BinOp::Gt => self.buf.write_str(" > "),
-            BinOp::Ge => self.buf.write_str(" >= "),
-            BinOp::Add => self.buf.write_str(" + "),
-            BinOp::Sub => self.buf.write_str(" - "),
-            BinOp::Mul => self.buf.write_str(" * "),
-            BinOp::Div => self.buf.write_str(" / "),
-            BinOp::And => self.buf.write_str(" && "),
-            BinOp::Or => self.buf.write_str(" || "),
-        };
+    fn visit_bin_op(&mut self, bin_op: &BinOp) -> fmt::Result {
+        match bin_op {
+            BinOp::Eq => self.write_str(" == "),
+            BinOp::Ne => self.write_str(" != "),
+            BinOp::Lt => self.write_str(" < "),
+            BinOp::Le => self.write_str(" <= "),
+            BinOp::Gt => self.write_str(" > "),
+            BinOp::Ge => self.write_str(" >= "),
+            BinOp::Add => self.write_str(" + "),
+            BinOp::Sub => self.write_str(" - "),
+            BinOp::Mul => self.write_str(" * "),
+            BinOp::Div => self.write_str(" / "),
+            BinOp::And => self.write_str(" && "),
+            BinOp::Or => self.write_str(" || "),
+        }
     }
 
-    fn visit_expr_group(&mut self, expr_group: &super::ExprGroup) {
-        self.parenthesize(Some("group"), |this| {
-            super::visitor::visit_expr_group(this, expr_group)
-        })
+    fn visit_expr_group(&mut self, expr_group: &super::ExprGroup) -> fmt::Result {
+        self.parenthesize(|this| super::visitor::visit_expr_group(this, expr_group))
     }
 
-    fn visit_expr_if(&mut self, expr_if: &super::ExprIf) {
-        let _ = self.buf.write_str("if ");
-        self.visit_expr(&expr_if.cond);
+    fn visit_expr_if(&mut self, expr_if: &super::ExprIf) -> fmt::Result {
+        self.write_str("if ")?;
+        self.visit_expr(&expr_if.cond)?;
 
-        let _ = self.buf.write_str(" do ");
-        self.visit_expr(&expr_if.do_branch);
+        self.write_str(" do")?;
+        self.newline()?;
+        self.indent();
+
+        self.visit_expr(&expr_if.do_branch)?;
 
         if let Some(expr) = expr_if.else_branch.as_ref() {
-            let _ = self.buf.write_str(" else ");
-            self.visit_expr(expr);
+            self.dedent();
+            self.newline()?;
+            self.write_str("else")?;
+
+            self.newline()?;
+            self.indent();
+            self.visit_expr(expr)?;
         }
-        let _ = self.buf.write_str(" end");
+
+        self.newline()?;
+        self.dedent();
+        self.write_str("end")
     }
 
-    fn visit_expr_app(&mut self, expr_app: &super::ExprApp) {
-        self.parenthesize(None, |this| {
-            this.visit_expr(&expr_app.func);
-            let _ = this.buf.write_str(" ");
-            this.visit_expr(&expr_app.arg);
-        });
+    fn visit_expr_app(&mut self, expr_app: &super::ExprApp) -> fmt::Result {
+        self.parenthesize(|this| {
+            this.visit_expr(&expr_app.func)?;
+            this.write_str(" ")?;
+            this.visit_expr(&expr_app.arg)
+        })
     }
 }
--- src/ast/visitor.rs
+++ src/ast/visitor.rs
@@ -5,69 +5,73 @@ use crate::ast::{
 };
 
 pub(crate) trait Visitor {
-    fn visit_ident(&mut self, ident: &Ident) {
-        visit_ident(self, ident);
+    type Err;
+
+    fn visit_ident(&mut self, ident: &Ident) -> Result<(), Self::Err> {
+        visit_ident(self, ident)
     }
 
-    fn visit_pat(&mut self, pat: &Pat) {
-        visit_pat(self, pat);
+    fn visit_pat(&mut self, pat: &Pat) -> Result<(), Self::Err> {
+        visit_pat(self, pat)
     }
 
-    fn visit_expr(&mut self, expr: &Expr) {
-        visit_expr(self, expr);
+    fn visit_expr(&mut self, expr: &Expr) -> Result<(), Self::Err> {
+        visit_expr(self, expr)
     }
 
-    fn visit_literal(&mut self, literal: &Literal) {
+    fn visit_literal(&mut self, literal: &Literal) -> Result<(), Self::Err> {
         visit_literal(self, literal)
     }
 
-    fn visit_expr_unary(&mut self, expr_unary: &ExprUnary) {
+    fn visit_expr_unary(&mut self, expr_unary: &ExprUnary) -> Result<(), Self::Err> {
         visit_expr_unary(self, expr_unary)
     }
 
-    fn visit_un_op(&mut self, un_op: &UnOp) {
+    fn visit_un_op(&mut self, un_op: &UnOp) -> Result<(), Self::Err> {
         visit_un_op(self, un_op)
     }
 
-    fn visit_expr_binary(&mut self, expr_binary: &ExprBinary) {
+    fn visit_expr_binary(&mut self, expr_binary: &ExprBinary) -> Result<(), Self::Err> {
         visit_expr_binary(self, expr_binary)
     }
 
-    fn visit_bin_op(&mut self, bin_op: &BinOp) {
+    fn visit_bin_op(&mut self, bin_op: &BinOp) -> Result<(), Self::Err> {
         visit_bin_op(self, bin_op)
     }
 
-    fn visit_expr_group(&mut self, expr_group: &ExprGroup) {
+    fn visit_expr_group(&mut self, expr_group: &ExprGroup) -> Result<(), Self::Err> {
         visit_expr_group(self, expr_group)
     }
 
-    fn visit_expr_if(&mut self, expr_if: &ExprIf) {
+    fn visit_expr_if(&mut self, expr_if: &ExprIf) -> Result<(), Self::Err> {
         visit_expr_if(self, expr_if)
     }
 
-    fn visit_expr_case(&mut self, expr_case: &ExprCase) {
+    fn visit_expr_case(&mut self, expr_case: &ExprCase) -> Result<(), Self::Err> {
         visit_expr_case(self, expr_case)
     }
 
-    fn visit_expr_let(&mut self, expr_let: &ExprLet) {
+    fn visit_expr_let(&mut self, expr_let: &ExprLet) -> Result<(), Self::Err> {
         visit_expr_let(self, expr_let)
     }
 
-    fn visit_expr_app(&mut self, expr_app: &ExprApp) {
+    fn visit_expr_app(&mut self, expr_app: &ExprApp) -> Result<(), Self::Err> {
         visit_expr_app(self, expr_app)
     }
 }
 
-pub(crate) fn visit_ident<V: Visitor + ?Sized>(_v: &mut V, _ident: &Ident) {}
+pub(crate) fn visit_ident<V: Visitor + ?Sized>(_v: &mut V, _ident: &Ident) -> Result<(), V::Err> {
+    Ok(())
+}
 
-pub(crate) fn visit_pat<V: Visitor + ?Sized>(v: &mut V, pat: &Pat) {
+pub(crate) fn visit_pat<V: Visitor + ?Sized>(v: &mut V, pat: &Pat) -> Result<(), V::Err> {
     match pat {
         Pat::Lit(expr_lit) => v.visit_literal(expr_lit),
         Pat::Ident(ident) => v.visit_ident(ident),
     }
 }
 
-pub(crate) fn visit_expr<V: Visitor + ?Sized>(v: &mut V, expr: &Expr) {
+pub(crate) fn visit_expr<V: Visitor + ?Sized>(v: &mut V, expr: &Expr) -> Result<(), V::Err> {
     match expr {
         Expr::Lit(literal) => v.visit_literal(literal),
         Expr::Ident(ident) => v.visit_ident(ident),
@@ -81,54 +85,88 @@ pub(crate) fn visit_expr<V: Visitor + ?Sized>(v: &mut V, expr: &Expr) {
     }
 }
 
-pub(crate) fn visit_literal<V: Visitor + ?Sized>(_v: &mut V, _literal: &Literal) {}
-
-pub(crate) fn visit_expr_unary<V: Visitor + ?Sized>(v: &mut V, expr_unary: &ExprUnary) {
-    v.visit_un_op(&expr_unary.op);
-    v.visit_expr(&expr_unary.expr);
+pub(crate) fn visit_literal<V: Visitor + ?Sized>(
+    _v: &mut V,
+    _literal: &Literal,
+) -> Result<(), V::Err> {
+    Ok(())
 }
 
-pub(crate) fn visit_un_op<V: Visitor + ?Sized>(_v: &mut V, _un_op: &UnOp) {}
-
-pub(crate) fn visit_expr_binary<V: Visitor + ?Sized>(v: &mut V, expr_binary: &ExprBinary) {
-    v.visit_expr(&expr_binary.lhs);
-    v.visit_bin_op(&expr_binary.op);
-    v.visit_expr(&expr_binary.rhs);
+pub(crate) fn visit_expr_unary<V: Visitor + ?Sized>(
+    v: &mut V,
+    expr_unary: &ExprUnary,
+) -> Result<(), V::Err> {
+    v.visit_un_op(&expr_unary.op)?;
+    v.visit_expr(&expr_unary.expr)
 }
 
-pub(crate) fn visit_bin_op<V: Visitor + ?Sized>(_v: &mut V, _bin_op: &BinOp) {}
-
-pub(crate) fn visit_expr_group<V: Visitor + ?Sized>(v: &mut V, expr_group: &ExprGroup) {
-    v.visit_expr(&expr_group.expr);
+pub(crate) fn visit_un_op<V: Visitor + ?Sized>(_v: &mut V, _un_op: &UnOp) -> Result<(), V::Err> {
+    Ok(())
 }
 
-pub(crate) fn visit_expr_if<V: Visitor + ?Sized>(v: &mut V, expr_if: &ExprIf) {
-    v.visit_expr(&expr_if.cond);
-    v.visit_expr(&expr_if.do_branch);
+pub(crate) fn visit_expr_binary<V: Visitor + ?Sized>(
+    v: &mut V,
+    expr_binary: &ExprBinary,
+) -> Result<(), V::Err> {
+    v.visit_expr(&expr_binary.lhs)?;
+    v.visit_bin_op(&expr_binary.op)?;
+    v.visit_expr(&expr_binary.rhs)
+}
+
+pub(crate) fn visit_bin_op<V: Visitor + ?Sized>(_v: &mut V, _bin_op: &BinOp) -> Result<(), V::Err> {
+    Ok(())
+}
+
+pub(crate) fn visit_expr_group<V: Visitor + ?Sized>(
+    v: &mut V,
+    expr_group: &ExprGroup,
+) -> Result<(), V::Err> {
+    v.visit_expr(&expr_group.expr)
+}
+
+pub(crate) fn visit_expr_if<V: Visitor + ?Sized>(
+    v: &mut V,
+    expr_if: &ExprIf,
+) -> Result<(), V::Err> {
+    v.visit_expr(&expr_if.cond)?;
+    v.visit_expr(&expr_if.do_branch)?;
     if let Some(expr) = expr_if.else_branch.as_ref() {
-        v.visit_expr(expr);
+        v.visit_expr(expr)?;
     }
+
+    Ok(())
 }
 
-pub(crate) fn visit_expr_case<V: Visitor + ?Sized>(v: &mut V, expr_case: &ExprCase) {
-    v.visit_expr(&expr_case.expr);
+pub(crate) fn visit_expr_case<V: Visitor + ?Sized>(
+    v: &mut V,
+    expr_case: &ExprCase,
+) -> Result<(), V::Err> {
+    v.visit_expr(&expr_case.expr)?;
 
     for (pat, expr) in &expr_case.arms {
-        v.visit_pat(pat);
-        v.visit_expr(expr);
+        v.visit_pat(pat)?;
+        v.visit_expr(expr)?;
     }
+
+    Ok(())
 }
 
-pub(crate) fn visit_expr_app<V: Visitor + ?Sized>(v: &mut V, expr_app: &ExprApp) {
-    v.visit_expr(&expr_app.func);
-    v.visit_expr(&expr_app.arg);
+pub(crate) fn visit_expr_app<V: Visitor + ?Sized>(
+    v: &mut V,
+    expr_app: &ExprApp,
+) -> Result<(), V::Err> {
+    v.visit_expr(&expr_app.func)?;
+    v.visit_expr(&expr_app.arg)
 }
 
-pub(crate) fn visit_expr_let<V: Visitor + ?Sized>(v: &mut V, expr_let: &ExprLet) {
-    v.visit_ident(&expr_let.lhs);
+pub(crate) fn visit_expr_let<V: Visitor + ?Sized>(
+    v: &mut V,
+    expr_let: &ExprLet,
+) -> Result<(), V::Err> {
+    v.visit_ident(&expr_let.lhs)?;
     for ident in &expr_let.args {
-        v.visit_ident(ident);
+        v.visit_ident(ident)?;
     }
-    v.visit_expr(&expr_let.rhs);
-    v.visit_expr(&expr_let.body);
+    v.visit_expr(&expr_let.rhs)?;
+    v.visit_expr(&expr_let.body)
 }
